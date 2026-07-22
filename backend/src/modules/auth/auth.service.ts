a4ccb3242dc14ee3af423d0ada8b9c1d09be0cf8
Gadri Mohamed <gadrimohamed8@gmail.com>
2026-07-22 12:38:39 +0100

    feat(auth): add login and authentication

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,10 +1,12 @@
 import { User } from "./auth.model.js";
 import { AppError } from "../../shared/errors/AppError.js";
-import type { RegisterInput } from "./auth.validation.js";
-
+import type { RegisterInput,LoginInput } from "./auth.validation.js";
+import { generateToken } from "../../utils/jwt.js";
+interface getMe {
+    userId:string
+}
 export async function registerUser(input: RegisterInput) {
   const existingUser = await User.findOne({ email: input.email });
-
   if (existingUser) {
     throw new AppError("Email is already registered", 409);
   }
@@ -25,3 +27,30 @@ export async function registerUser(input: RegisterInput) {
     role: user.role,
   };
 }
+export async function loginUser(input: LoginInput) {
+  const user = await User.findOne({ email: input.email }).select("+password");
+
+  if (!user) {
+    throw new AppError("Invalid email or password", 401);
+  }
+
+  const isPasswordValid = await user.comparePassword(input.password);
+
+  if (!isPasswordValid) {
+    throw new AppError("Invalid email or password", 401);
+
+  }
+    const token = generateToken({ userId: user._id.toString() });
+
+  return {
+    token,
+    id: user._id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    role: user.role,
+  };
+}
+// export async function getMeUser(input:getMe){
+
+// }
---
+++ backend/src/shared/types/express.d.ts
@@ -0,0 +1,11 @@
+import type { IUser } from "../../modules/auth/auth.model.js";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: IUser;
+    }
+  }
+}
+
+export {};  
